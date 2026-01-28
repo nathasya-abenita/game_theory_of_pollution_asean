0c45856ebb5ffc3d01f600a70bb07e5bdda3a137
Nathasya Christien <nathasya.abenita@gmail.com>
2026-01-28 11:40:55 +0100

    finalize features for network script

--- src/main/03_make_countries_network.py
+++ src/main/03_make_countries_network.py
@@ -7,7 +7,7 @@ from shapely.geometry import Point, LineString
 import rasterio.features
 import array_to_latex as a2l
 
-plt.style.use('dark_background') # use dark mode style
+# plt.style.use('dark_background') # use dark mode style
 
 crs_short = 32649
 crs_long = "EPSG:32649"
@@ -73,6 +73,66 @@ def read_wind_raster_monthly (i):
     raster_v_arr = read_wind_raster_specific("v", str(i))
     return raster_u_arr, raster_v_arr
 
+def compute_wind_speed_mean (geom, raster_wind_u, raster_wind_v, raster_pop):
+    mask = rasterio.features.rasterize(
+                [(geom, 1)],
+                out_shape=raster_wind_u.data.shape,
+                transform=raster_wind_u.rio.transform(),
+                fill=0,
+                all_touched=True
+            ).astype(bool)
+
+    valid = mask & (raster_pop.data >= 0)
+
+    # Compute u-component
+    w = raster_pop.data[valid]
+    u_weighted = (w * raster_wind_u.data[valid]).sum() / w.sum()
+    v_weighted = (w * raster_wind_v.data[valid]).sum() / w.sum()
+    return u_weighted, v_weighted
+
+def compute_speed_proportion (wind_vector_list_per_month, month_id, country_id, s_max = 4.3):
+    wind_vector = np.array(wind_vector_list_per_month[month_id][country_id])
+    speed = np.sqrt(np.sum(wind_vector**2))
+    if speed < s_max:
+        p = speed/s_max
+    else:
+        p = 1
+    return p
+
+def compute_angle_proportion (wind_vector_list_per_month, centroids, month_id, country_id_i, country_id_j):
+    # Take wind vector
+    w = wind_vector_list_per_month[month_id][country_id_i]
+    w_x = w[0]
+    w_y = w[1]
+
+    # Compute distances on x- and y-axis
+    x_delta = centroids[country_id_j].x - centroids[country_id_i].x
+    y_delta = centroids[country_id_j].y - centroids[country_id_i].y
+
+    # Compute dot product
+    dot_p = (w_x * x_delta) + (w_y * y_delta)
+    dot_p = dot_p / np.sqrt((w_x**2+w_y**2)*(x_delta**2+y_delta**2))
+
+    # Apply function for angle proportion based on dot product
+    if dot_p >= 0:
+        p = dot_p
+    else:
+        p = 0
+    return p
+
+def compute_distance_proportion (centroids, country_id_i, country_id_j, d_max = 1_964_972.7):
+
+    x_delta = centroids[country_id_i].x - centroids[country_id_j].x
+    y_delta = centroids[country_id_i].y - centroids[country_id_j].y
+    norm = np.sqrt(x_delta**2 + y_delta**2)
+
+    if norm <= d_max:
+        p = np.cos(norm/d_max * np.pi/2)
+        #p = norm/d_max
+    else:
+        p = 0
+    return p
+
 if __name__ == '__main__':    
     """ 1.1 Read countries polygons and set index """
 
@@ -109,10 +169,10 @@ if __name__ == '__main__':
     centroids_population, lines = create_graph_vertices(raster_pop, X, Y, gdf_countries)
 
     # Plotting
-    base = gdf_countries.plot(cmap = "tab10")
-    gdf_countries.centroid.plot(ax = base, marker = 'o', color = 'yellow', markersize = 15, label = "Basic centroid")
-    centroids_population.plot(ax = base, marker = '*', color = 'yellow', markersize = 15, label = "Population-weighted centroid")
-    lines.plot(ax = base, color = "yellow")
+    base = gdf_countries.plot(cmap = "pink")
+    gdf_countries.centroid.plot(ax = base, marker = 'o', color = 'blue', markersize = 15, label = "Basic centroid")
+    centroids_population.plot(ax = base, marker = 'o', color = 'red', markersize = 15, label = "Population-weighted centroid")
+    lines.plot(ax = base, color = "orange")
 
     # Decoration
     plt.title("Centroids of ASEAN Countries")
@@ -138,22 +198,8 @@ if __name__ == '__main__':
         # Iterate over each country's polygon
         wind_vector_list = []
         for geom in gdf_countries.geometry:
-
-            mask = rasterio.features.rasterize(
-                [(geom, 1)],
-                out_shape=raster_wind_u.data.shape,
-                transform=raster_wind_u.rio.transform(),
-                fill=0,
-                all_touched=True
-            ).astype(bool)
-
-            valid = mask & (raster_pop.data >= 0)
-
-            # Compute u-component
-            w = raster_pop.data[valid]
-            u_weighted = (w * raster_wind_u.data[valid]).sum() / w.sum()
-            v_weighted = (w * raster_wind_v.data[valid]).sum() / w.sum()
-
+            # Compute mean and save it
+            u_weighted, v_weighted = compute_wind_speed_mean(geom, raster_wind_u, raster_wind_v, raster_pop)
             wind_vector_list.append([u_weighted, v_weighted])
 
         # Save information
@@ -165,7 +211,7 @@ if __name__ == '__main__':
     fig, axs = plt.subplots(4,3, figsize=(10,10),tight_layout=True)
     for i in range (4):
         for j in range (3):
-            gdf_countries.plot(ax = axs[i][j], cmap = "tab10") # cmap pink juga bagus
+            gdf_countries.plot(ax = axs[i][j], cmap = "pink") # cmap pink or tab10
             centroids_population.plot(ax = axs[i][j], marker = 'o', color = 'blue', markersize = 20)
 
     
@@ -185,12 +231,54 @@ if __name__ == '__main__':
             qv = ax.quiver(centroids_population[i].x, centroids_population[i].y,
                     wind_vector_per_month_list[time_int_id][i][0],
                     wind_vector_per_month_list[time_int_id][i][1],
-                    color='white') # darkturquoise
+                    color='darkturquoise') # white
             # Add legend of the quiver
-            qk = ax.quiverkey(qv, 1, 1, 1, r'$1 \frac{m}{s}$', labelpos='E',
-                   coordinates='figure')
+            legend_arrow_length = 1
+            qk = ax.quiverkey(qv, 1, 1, legend_arrow_length, rf'{legend_arrow_length} m/s', labelpos='E',
+                   coordinates='axes')
         # Modify plotting style
         ax.axis("off")
         ax.set_title(time_int_strings[time_int_id], fontsize=10)
     plt.tight_layout()
     plt.show()
+
+    """ 3.4. Save wind vectors values """
+    np.save("./output/wind_vector_per_month_list.npy", wind_vector_per_month_list)
+
+    """ 4.1. Compute psi_{ij} or graph's weight """
+
+    psi_list = [] # indexes: month, country_i, contry_j
+    # Iterate over each month
+    for month_id in range (12): # 12 months
+        # Initialize 2-by-2 matrix for country-i ot country-j
+        psi = np.zeros((len(gdf_countries), len(gdf_countries)))
+        for country_id_i in range (len(gdf_countries)):
+            for country_id_j in range (len(gdf_countries)):
+                if country_id_i == country_id_j:
+                    p = 0
+                else:
+                    p_speed = compute_speed_proportion (wind_vector_per_month_list, month_id, country_id_i)
+                    p_angle = compute_angle_proportion (wind_vector_per_month_list, centroids_population, month_id, country_id_i, country_id_j)
+                    p_distance = compute_distance_proportion (centroids_population, country_id_i, country_id_j)
+                    p = p_speed * p_angle * p_distance
+                psi[country_id_i][country_id_j] = p
+        # Save matrix for that month
+        psi_list.append(psi)
+    
+    """ 4.2. Save psi list  """
+    np.save("./output/psi_list.npy", psi_list)
+
+    """ 4.3. Print psi list in LaTeX """
+
+    with open("./output/psi_list.txt", "w") as f:
+        for i in range(12):
+            f.write(f"month {i+1}\n")
+            latex_str = a2l.to_ltx(
+                psi_list[i],
+                frmt='{:6.2f}',
+                arraytype='bmatrix',
+                print_out=False
+            )
+            f.write(latex_str + "\n\n")
+
+    
